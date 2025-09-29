ea7d1691b1b527a231b2a09f50e66f9d89cf51e3
Shirayuki39 <lorddemecrius83@proton.me>
2025-09-29 21:27:19 +0900

    LG7n: Sync with device tree
    Signed-off-by: Shirayuki39 <lorddemecrius83@proton.me>

--- Android.mk
+++ Android.mk
@@ -3,3 +3,21 @@
 #
 
 LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),LG7n)
+
+$(call add-radio-file-sha1-checked,radio/dpm.img,b4d35e7c091a10e59eafd3e53a1467a53a303e9d)
+$(call add-radio-file-sha1-checked,radio/dtbo.img,3faa1514b7bc80688b4729da8ef97cc7b40cf135)
+$(call add-radio-file-sha1-checked,radio/gz.img,700586132f96729f9119707bd08d65c8f765d457)
+$(call add-radio-file-sha1-checked,radio/lk.img,8ce79dd106ba75b1a1c433775e532004418ab43e)
+$(call add-radio-file-sha1-checked,radio/logo.bin,1ad48d8fa7dde953c33799ed64370b14479c1017)
+$(call add-radio-file-sha1-checked,radio/mcupm.img,033dc74a905ca54b692e6bda4549607cc2c46ec3)
+$(call add-radio-file-sha1-checked,radio/md1img.img,39564160d2d4646c580d09134c7986f66779c9e8)
+$(call add-radio-file-sha1-checked,radio/pi_img.img,9e7782e84e3f942d3ca2432dd7d3b9b63ef5d275)
+$(call add-radio-file-sha1-checked,radio/scp.img,5d700d3e356cc195b3d085e9dc9ddebc68a9f9c8)
+$(call add-radio-file-sha1-checked,radio/spmfw.img,51c5356ce135825577217f75a6162cae27dded3b)
+$(call add-radio-file-sha1-checked,radio/sspm.img,0bc0bc3f912c11bf89f6ec9bc68c0f0b6e023245)
+$(call add-radio-file-sha1-checked,radio/tee.img,b4103c705d7261ea82a13a4a51fddbbf32ad018e)
+$(call add-radio-file-sha1-checked,radio/tkv.img,4a698f0035f39d716a397f57e9c74b68f118b4c2)
+
+endif
